598c3ea460f8f30e5901eb2d6b77f1b24869c2fd
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-10 07:29:20 -0800

    fix:pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Fabric script that generates a .tgz archive """
-
 from fabric.api import local
 from datetime import datetime
 import os
+
 
 def do_pack():
     """ generates a .tgz archive from the content of web_static folder """
@@ -14,9 +14,7 @@ def do_pack():
                 now.year, now.month, now.day, now.hour, now.minute, now.second
                 )
         archive_path = "versions/{}".format(archive_name)
-
         local("tar -cvaf {} web_static".format(archive_path))
-        
         return (archive_path)
 
     except Exception as e:
