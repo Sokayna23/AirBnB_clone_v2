c65e1be548047bee92f719fa74f651c27d8e3188
sokayna <sokayna.bennasseur@gmail.com>
2024-02-12 11:59:49 -0500

    task2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,6 +30,6 @@ echo "server {
    location = /404.html{
                  internal;
          }
-}" > /etc/nginx/sites-available/default
+}" | sudo tee /etc/nginx/sites-available/default
 sudo nginx -t
 sudo service nginx restart
