b82b34c442d073aca5ab30d9afef527007709102
Sokayna <sokayna.bennasseur@gmail.com>
2024-02-14 11:34:51 -0800

    100

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,88 +1,29 @@
-# Server configuration
-
-$conf = "server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-        add_header X-Served-By ${hostname};
-
-        location /hbnb_static {
-                alias /data/web_static/current/;
-        }
-        error_page 404 /custom_404.html;
-        location = /custom_404.html {
-                 root /var/www/html;
-                 internal;
-        }
-        location /redirect_me {
-                 return 301 https://www.youtube.com/watch?v=3lFkDc6dFoY;
-        }
-}"
-
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
+# instal and configure nginx
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
--> file { '/data':
-  ensure  => 'directory'
+-> package { 'nginx':
+  ensure => installed,
 }
-
--> file { '/data/web_static':
-  ensure => 'directory'
+-> exec { 'run1':
+  command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
 }
-
--> file { '/data/web_static/releases':
-  ensure => 'directory'
+-> exec { 'run2':
+  command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
 }
-
--> file { '/data/web_static/releases/test':
-  ensure => 'directory'
+-> exec { 'run3':
+  command => '/usr/bin/rm -rf /data/web_static/current',
 }
-
--> file { '/data/web_static/shared':
-  ensure => 'directory'
+-> exec { 'run4':
+  command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
 }
-
--> file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School\n"
+-> exec { 'run5':
+  command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
-
--> file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
+-> exec { 'hbnb_static':
+  command => 'sudo sed -i "/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}" /etc/nginx/sites-enabled/default',
+  provider => shell,
 }
-
--> exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
-
-file { '/var/www':
-  ensure => 'directory'
-}
-
--> file { '/var/www/html':
-  ensure => 'directory'
-}
-
--> file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School Nginx\n"
-}
-
--> file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-}
-
--> file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $conf
-}
-
--> service {'nginx':
-  ensure  => 'running',
-  require => Package['nginx'],
+-> exec { 'run6':
+  command => '/usr/sbin/service nginx restart',
 }
